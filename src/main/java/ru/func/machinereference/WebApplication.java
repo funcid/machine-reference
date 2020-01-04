5301442811e4469cb3afa5683dc58ae5f68875bb
DmitriyMX <dimon550@gmail.com>
2020-01-04 21:06:27 +0300

    Spring Boot 2

--- src/main/java/ru/func/machinereference/WebApplication.java
+++ src/main/java/ru/func/machinereference/WebApplication.java
@@ -1,58 +1,14 @@
 package ru.func.machinereference;
 
-import lombok.extern.slf4j.Slf4j;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.log.Log;
-import org.eclipse.jetty.util.log.Slf4jLog;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-import ru.func.machinereference.config.DatabaseConfig;
-import ru.func.machinereference.config.MvcConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.net.InetSocketAddress;
-
-@Slf4j
+@SpringBootApplication
+@EnableJpaRepositories
 public class WebApplication {
 
-    private void startJetty(String host, int port) {
-        Log.setLog(new Slf4jLog("Jetty.Logger"));
-
-        Server server = new Server(new InetSocketAddress(host, port));
-        server.setHandler(createServletContextHandler(createWebApplicationContext()));
-        try {
-            server.start();
-            server.join();
-        } catch (Exception e) {
-            log.error("Error start server", e);
-        }
-    }
-
-    private ServletContextHandler createServletContextHandler(WebApplicationContext context) {
-        ServletContextHandler contextHandler = new ServletContextHandler();
-        contextHandler.setErrorHandler(null);
-        contextHandler.setContextPath("/");
-        contextHandler.addServlet(new ServletHolder(new DispatcherServlet(context)), "/*");
-        contextHandler.addEventListener(new ContextLoaderListener(context));
-        return contextHandler;
-    }
-
-    private WebApplicationContext createWebApplicationContext() {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.register(MvcConfig.class);
-        applicationContext.register(DatabaseConfig.class);
-
-        return applicationContext;
-    }
-
     public static void main(String[] args) {
-        final String host = System.getProperty("host", "127.0.0.1");
-        final int port = Integer.parseInt(System.getProperty("port", "8080"));
-
-        log.info("Web app listen: {}:{}", host, port);
-        new WebApplication().startJetty(host, port);
+        SpringApplication.run(WebApplication.class, args);
     }
 }
--- src/main/java/ru/func/machinereference/config/MvcConfig.java
+++ src/main/java/ru/func/machinereference/config/MvcConfig.java
@@ -1,52 +1,14 @@
 package ru.func.machinereference.config;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.ISpringTemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("ru.func.machinereference")
 public class MvcConfig implements WebMvcConfigurer {
     private static final int ONE_YEAR = 365*24*60*60;
-
-    @Bean
-    public ITemplateResolver templateResolver(ApplicationContext applicationContext){
-        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
-        resolver.setApplicationContext(applicationContext);
-        resolver.setPrefix("classpath:/templates/");
-        resolver.setTemplateMode(TemplateMode.HTML);
-        resolver.setCacheable(true);
-        resolver.setSuffix(".html");
-        return resolver;
-    }
-
-    @Bean
-    public ISpringTemplateEngine templateEngine(ITemplateResolver templateResolver){
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver);
-        return engine;
-    }
-
-    @Bean
-    public ViewResolver viewResolver(ISpringTemplateEngine templateEngine) {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine);
-        viewResolver.setContentType("text/html;charset=UTF-8");
-        viewResolver.setCache(true);
-        return viewResolver;
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
